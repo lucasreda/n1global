d3e3e7bdd354dec49d1c4f5d78941e5c5b7b0c6b
Lucas Alencar <lucasreda@gmail.com>
2025-11-15 18:57:21 -0300

    fix: corrigir build do Railway - remover --packages=external e adicionar server/ e scripts/ ao Git

--- client/src/lib/history/index.ts
+++
@@ -1,4 +0,0 @@
-export type { Command, HistoryState } from './Command';
-export { UpdateModelCommand, MAX_HISTORY_SIZE } from './Command';
-export { HistoryManager } from './HistoryManager';
-export { useHistoryManager } from './useHistoryManager';
